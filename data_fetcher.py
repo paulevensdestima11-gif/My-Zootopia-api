c59bb34b68e2c4982fd0947e9c0e6b4726e8647b
Paul <paulevensdestima11@gmail.com>
2026-05-09 01:55:45 +0200

    adding docstring

--- data_fetcher.py
+++ data_fetcher.py
@@ -2,6 +2,22 @@ import requests
 
 
 def fetch_data(animal_name):
+    """
+     Fetches the animals data for the animal 'animal_name'.
+     Returns: a list of animals, each animal is a dictionary:
+     {
+       'name': ...,
+       'taxonomy': {
+         ...
+       },
+       'locations': [
+         ...
+       ],
+       'characteristics': {
+         ...
+       }
+     },
+     """
 
     url = f"https://api.api-ninjas.com/v1/animals?name={animal_name}"
 
