f2d0b54be408324b934828fac029729cc977bb4d
Paul <paulevensdestima11@gmail.com>
2026-05-09 02:07:51 +0200

    adding the constant

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,5 +1,8 @@
 import requests
 
+API_KEY = "tb87Gu7Kw3IcRFoDhqSX6cZuSPfEZVFCPw4X2VNR"
+API_URL = "https://api.api-ninjas.com/v1/animals"
+
 
 def fetch_data(animal_name):
     """
@@ -19,10 +22,10 @@ def fetch_data(animal_name):
      },
      """
 
-    url = f"https://api.api-ninjas.com/v1/animals?name={animal_name}"
+    url = f"{API_URL}?name={animal_name}"
 
     headers = {
-        "X-Api-Key": "YOUR_API_KEY"
+        "X-Api-Key": API_KEY
     }
 
     response = requests.get(url, headers=headers)
